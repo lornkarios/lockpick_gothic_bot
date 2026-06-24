f1ddb6c39d606c8ef4ff8e044ce7d09f95e1d830
antkovalev <antkovalev@azbukavkusa.ru>
2026-06-24 12:08:55 +0300

    fix deploy

--- deploy_scripts/php/deploy.sh
+++ deploy_scripts/php/deploy.sh
@@ -7,7 +7,8 @@ echo "Pull image"
 docker pull lornkarios/lockpick-gothic-api-bot
 echo "Start php container"
 cd /root/lockpick_gothic_bot/php
-docker run
+docker run \
   --network=lockpick_gothic_bot_network \
-  lornkarios/lockpick-gothic-api-bot
+  --name=php \
+  lornkarios/lockpick-gothic-api-bot -d
 echo "Finish deploying!"
